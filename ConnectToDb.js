cb9d418db670758c8a2c903828d1514cf39ad310
LaylaJAlachkar <72898713+LaylaJAlachkar@users.noreply.github.com>
2020-12-01 21:34:16 -0500

    Testing

--- ConnectToDb.js
+++ ConnectToDb.js
@@ -2,15 +2,18 @@ const MongoClient = require('mongodb').MongoClient;
 const uri = "mongodb+srv://Website:Password1@cluster0.cykhi.mongodb.net/PersonalWebsite?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true });
 
-
+function tester() {
+	document.write("Hello")
+}
 function getMostRecent() {
+	document.write("Hello"); 
 	client.connect(err => {
 	  const recipe = client.db("PersonalWebsite").collection("tblRecipe");
 	  const ingredient = client.db("PersonalWebsite").collection("tblIngredient");
 	  const instruction = client.db("PersonalWebsite").collection("tblInstruction");
 	  // perform actions on the collection object
 	  var mySort = {When: -1}
-	  echo "Hello"
+	  document.write("Hello"); 
 	  recipe.findOne().sort(mySort).toArray(function(err, result) {
 		  if(err) throw err;
 		  each recip in recipe
